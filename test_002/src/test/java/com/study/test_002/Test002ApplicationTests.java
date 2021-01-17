3f757b9c75d1d4eab1131b88c5c2f869d3418069
YoXung <89034550@qq.com>
2021-01-18 02:40:04 +0800

    test002 commit

---
+++ test_002/src/test/java/com/study/test_002/Test002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.test_002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

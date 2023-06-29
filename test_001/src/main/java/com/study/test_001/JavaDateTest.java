dadd84d3295131141eae564b20b8bcc7c3e9aeb0
YoXung <89034550@qq.com>
2023-06-29 21:21:28 +0800

    修改maven

--- test_001/src/main/java/com/study/test_001/JavaDateTest.java
+++ test_001/src/main/java/com/study/test_001/JavaDateTest.java
@@ -2,7 +2,6 @@ package com.study.test_001;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -17,15 +16,15 @@ import java.util.Locale;
 public class JavaDateTest {
 
     public static void main(String[] args) {
-        LocalDateTime dateTime = LocalDateTime.now();
-        log.info("LocalDateTime得到的原始日期时间为：{}", dateTime);
+        LocalDateTime date1 = LocalDateTime.now();
+        log.info("LocalDateTime得到的原始日期时间为：{}", date1);
+        // java8新特性：https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/time/format/DateTimeFormatter.html
         // 日期转字符
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss a", Locale.ENGLISH);
-        String str = dateTime.format(formatter1);
-        log.info("12/24小时日期时间转字符串（带AM/PM）结果为：{}", str);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss a", Locale.ENGLISH);
+        String dateStr = date1.format(formatter);
+        log.info("12/24小时日期时间转字符串（带AM/PM）结果为：{}", dateStr);
         // 字符转日期
-        LocalDateTime date2 = LocalDateTime.parse(str, formatter1);
+        LocalDateTime date2 = LocalDateTime.parse(dateStr, formatter);
         log.info("字符串转日期结果为：{}", date2);
-
     }
 }
